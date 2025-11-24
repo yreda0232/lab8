1175bf5cab578d2af9fe6feb6e67ac53d0ef9ec1
youssef reda <yreda0232@gmail.com>
2025-11-24 03:05:24 +0200

    .

--- src/project7/Course.java
+++ src/project7/Course.java
@@ -94,9 +94,7 @@ public Lesson getLessonById(String lessonId) {
 
     
     
-    public String getLastModifiedBy() {return lastModifiedBy;}
-
-    public String getLastStatusChange() {return lastStatusChange;}
+    
     
     
     public void editCourse(String newTitle, String newDescription, String newInstructorId) {
